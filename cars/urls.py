d78d433958202dea0c2046eed7f69397fd4903ca
nythan5 <g.n.d2014@gmail.com>
2024-06-18 17:41:01 -0300

    Finalizando aula 73 | Ajustes no template

--- cars/urls.py
+++ cars/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.CarListView.as_view(), name='list'),
     path('new_car/', views.CarCreateView.as_view(), name='create'),
     path('detail/<int:pk>', views.CarDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', views.CarUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', views.CarDeleteView.as_view(), name='delete'),
 
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,10 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from .models import Car
 from .forms import CarForm
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 
 # Create your views here.
@@ -35,4 +35,22 @@ class CarCreateView(CreateView):
 class CarDetailView(DetailView):
     model = Car
     template_name = 'detail.html'
-    context_object_name = 'car'
+    context_object_name = 'car'  # se nao colocar context_object_name o objeto dentro do template se chamará "objetc" # noqa
+
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarForm
+    template_name = 'update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('car:detail', kwargs={'pk': self.object.pk})
+
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'delete.html'
+    context_object_name = 'car'  # se nao colocar context_object_name o objeto dentro do template se chamará "objetc" # noqa
+
+    def get_success_url(self):
+        return reverse('car:list')  # precisamos sobreescrever o success_url para usar o REVERSE # noqa
